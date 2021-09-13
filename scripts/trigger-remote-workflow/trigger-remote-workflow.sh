298d5dee6d63af36fb6daf6574d320301ebe2166
Hippy <hippyod@hippys-bfs.localdomain>
2021-09-13 15:46:45 -0500

    first pass testing remote, centralized build workflow triggering

---
+++ scripts/trigger-remote-workflow/trigger-remote-workflow.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+# script to trigger Github Actions workflow in held in this repository
+#
+# GITHUB_REPOSITORY and GITHUB_REF will be predefined by the GitHub Actions platform
+#
+# repos wishing to execute workflows defined in the cicd-infra repo will need a 
+# cicd-infra PAT (CICD_INFRA_SVC_ACCT_PAT) added (automagically handled during onboarding)
+
+set -x
+WORKFLOW_NAME=$1
+CICD_INFRA_SVC_ACCT_PAT=$2 
+
+PAYLOAD='{"ref":"master",
+          "inputs": {
+            "repo": "'${GITHUB_REPOSITORY}'",
+            "ref": "'${GITHUB_REF}'"
+          }
+        }'
+
+ACCEPT_HEADER='Accept: application/vnd.github.v3+json'
+AUTHORIZATION_HEADER="Authorization: token ${CICD_INFRA_SVC_ACCT_PAT}"
+
+REMOTE_WORKFLOW_CICD_INFRA_URL=https://api.github.com/repos/hippyod/cicd-infra/actions/workflows/${WORKFLOW_NAME}.yml/dispatches
+
+curl -X POST -H "${ACCEPT_HEADER}" -H "${AUTHORIZATION_HEADER}" "${REMOTE_WORKFLOW_CICD_INFRA_URL}" -d "${PAYLOAD}"
+set +x
